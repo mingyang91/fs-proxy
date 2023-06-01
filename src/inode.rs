feae15c3c644e7a902bb1e41dd64578361dd12a5
mingyang <my@famer.me>
2023-06-01 15:08:32 +0800

    compile success

--- src/inode.rs
+++ src/inode.rs
@@ -32,6 +32,13 @@ impl INode {
     }
   }
 
+  pub fn get_ino(&self) -> u64 {
+    match self {
+      INode::File { ino, .. } => *ino,
+      INode::Folder { ino, .. } => *ino,
+    }
+  }
+
   fn set_ino(&mut self, ino: u64) {
     match self {
       INode::File { ino: i, .. } => *i = ino,
@@ -73,16 +80,11 @@ impl INodeOps for Rc<RefCell<INode>> {
     match self.borrow().deref() {
       INode::File { .. } => vec![self.clone()],
       INode::Folder { entries, .. } => {
-        entries.values()
-          .flat_map(|entry| match *entry.borrow() {
-            INode::File { .. } => vec![entry.clone()],
-            INode::Folder { .. } => {
-              let mut files = entry.clone().list_recursively();
-              files.push(entry.clone());
-              files
-            },
-          })
-          .collect()
+        let mut res: Vec<Rc<RefCell<INode>>> = entries.values()
+          .flat_map(|entry| entry.list_recursively())
+          .collect();
+        res.push(self.clone());
+        res
       }
     }
   }
@@ -125,70 +127,89 @@ impl From<Rc<RefCell<INode>>> for INodeTable {
   }
 }
 
-fn fake_inode_tree() -> INode {
-  let hosts = Rc::new(RefCell::new(INode::File {
-    ino: 0,
-    name: String::from("hosts"),
-    target: String::from("/etc/hosts"),
-  }));
-  let passwd = Rc::new(RefCell::new(INode::File {
-    ino: 0,
-    name: String::from("passwd"),
-    target: String::from("/etc/passwd"),
-  }));
-  let shadow = Rc::new(RefCell::new(INode::File {
-    ino: 0,
-    name: String::from("shadow"),
-    target: String::from("/etc/shadow"),
-  }));
-  let group = Rc::new(RefCell::new(INode::File {
-    ino: 0,
-    name: String::from("group"),
-    target: String::from("/etc/group"),
-  }));
-  let subfile = Rc::new(RefCell::new(INode::File {
-    ino: 0,
-    name: String::from("subfile"),
-    target: String::from("/etc/subfile"),
-  }));
-  let subfolder = Rc::new(RefCell::new(INode::Folder {
-    ino: 0,
-    name: String::from("subfolder"),
-    entries: {
-      let mut es = BTreeMap::new();
-      es.insert(String::from("subfile"), subfile);
-      es
-    },
-  }));
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::fs;
 
-  let etc = Rc::new(RefCell::new(INode::Folder {
-    ino: 0,
-    name: String::from("etc"),
-    entries: {
-      let mut es = BTreeMap::new();
-      es.insert(String::from("hosts"), hosts);
-      es.insert(String::from("passwd"), passwd);
-      es.insert(String::from("shadow"), shadow);
-      es.insert(String::from("group"), group);
-      es.insert(String::from("subfolder"), subfolder);
-      es
-    },
-  }));
-  INode::Folder {
-    ino: 0,
-    name: String::from("/"),
-    entries: {
-      let mut es = BTreeMap::new();
-      es.insert(String::from("etc"), etc);
-      es
-    },
+
+  fn fake_inode_tree() -> INode {
+    let hosts = Rc::new(RefCell::new(INode::File {
+      ino: 0,
+      name: String::from("hosts"),
+      target: String::from("/etc/hosts"),
+    }));
+    let passwd = Rc::new(RefCell::new(INode::File {
+      ino: 0,
+      name: String::from("passwd"),
+      target: String::from("/etc/passwd"),
+    }));
+    let shadow = Rc::new(RefCell::new(INode::File {
+      ino: 0,
+      name: String::from("shadow"),
+      target: String::from("/etc/shadow"),
+    }));
+    let group = Rc::new(RefCell::new(INode::File {
+      ino: 0,
+      name: String::from("group"),
+      target: String::from("/etc/group"),
+    }));
+    let subfile = Rc::new(RefCell::new(INode::File {
+      ino: 0,
+      name: String::from("subfile"),
+      target: String::from("/etc/subfile"),
+    }));
+    let subfolder = Rc::new(RefCell::new(INode::Folder {
+      ino: 0,
+      name: String::from("subfolder"),
+      entries: {
+        let mut es = BTreeMap::new();
+        es.insert(String::from("subfile"), subfile);
+        es
+      },
+    }));
+
+    let etc = Rc::new(RefCell::new(INode::Folder {
+      ino: 0,
+      name: String::from("etc"),
+      entries: {
+        let mut es = BTreeMap::new();
+        es.insert(String::from("hosts"), hosts);
+        es.insert(String::from("passwd"), passwd);
+        es.insert(String::from("shadow"), shadow);
+        es.insert(String::from("group"), group);
+        es.insert(String::from("subfolder"), subfolder);
+        es
+      },
+    }));
+    INode::Folder {
+      ino: 0,
+      name: String::from("/"),
+      entries: {
+        let mut es = BTreeMap::new();
+        es.insert(String::from("etc"), etc);
+        es
+      },
+    }
   }
-}
 
-#[test]
-fn test() {
-  let root = Rc::new(RefCell::new(fake_inode_tree()));
+  #[test]
+  fn test() {
+    let root = Rc::new(RefCell::new(fake_inode_tree()));
 
-  let table = INodeTable::from(root);
-  println!("list_files: {:?}", table);
+    let table = INodeTable::from(root);
+    println!("list_files: {:?}", table);
+  }
+
+  #[test]
+  fn test_mapping_file() {
+    let mapping_tree = fs::read_to_string("res/mapping-tree.json").unwrap();
+    let path: Path = serde_json::from_str(&mapping_tree).unwrap();
+    let root = Rc::new(RefCell::new(path.into()));
+    let table = INodeTable::from(root);
+    let root_ino = table.root.borrow().get_ino();
+    let file = table.lookup(root_ino, String::from("Surge.app"));
+
+    assert!(file.is_some());
+  }
 }
